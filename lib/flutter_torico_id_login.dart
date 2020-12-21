2ec22c3fe05f403f45682d771b19342673c3a4f0
0maru <0maru.dev@gmail.com>
2020-12-21 18:42:00 +0900

    export

--- lib/flutter_torico_id_login.dart
+++ lib/flutter_torico_id_login.dart
@@ -1,14 +1,1 @@
-
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class FlutterToricoIdLogin {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_torico_id_login');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
+export './src/torico_id_login.dart';
