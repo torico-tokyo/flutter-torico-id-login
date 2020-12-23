1301fd832dc7df1625022b7fe1b8d352f4164e90
0maru <0maru.dev@gmail.com>
2020-12-23 18:16:36 +0900

    android Intent Listener

--- android/src/main/kotlin/com/maru/flutter_torico_id_login/FlutterToricoIdLoginPlugin.kt
+++ android/src/main/kotlin/com/maru/flutter_torico_id_login/FlutterToricoIdLoginPlugin.kt
@@ -1,36 +1,107 @@
 package com.maru.flutter_torico_id_login
 
+import android.content.Intent
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.EventChannel.EventSink
+import io.flutter.plugin.common.EventChannel.StreamHandler
+import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import io.flutter.plugin.common.PluginRegistry.NewIntentListener
+import java.sql.DriverManager.println
+
+const val METHOD_NAME: string = "torico/flutter_torico_id_login"
 
 /** FlutterToricoIdLoginPlugin */
-class FlutterToricoIdLoginPlugin: FlutterPlugin, MethodCallHandler {
+class FlutterToricoIdLoginPlugin: FlutterPlugin, MethodCallHandler, ActivityAware, NewIntentListener {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+  private var methodChannel : MethodChannel? = null
+  private var eventChannel: EventChannel? = null
+  private var eventSink: EventSink? = null
+  private var activityPluginBinding: ActivityPluginBinding? = null
+  private var scheme: string? = ""
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_torico_id_login")
-    channel.setMethodCallHandler(this)
+  companion object {
+    @JvmStatic
+    fun registerWith(registrar: Registrar) {
+      val channel = MethodChannel(registrar.messenger(), METHOD_NAME)
+      FlutterToricoIdLoginPlugin().onAttachedToEngine(registrar.messenger())
+    }
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    if (call.method == "setScheme") {
+      scheme = call.arguments as String
+      result.success(null)
     } else {
       result.notImplemented()
     }
   }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+  fun onAttachedToEngine(messanger: BinaryMessenger) {
+    methodChannel = MethodChannel(messanger, METHOD_NAME)
+    methodChannel!!.setMethodCallHandler(this)
+
+    eventChannel = EventChannel(messenger, "${METHOD_NAME}/event")
+    eventChannel!!.setStreamHandler(object: StreamHandler {
+      override fun onListen(arguments: Any?, events: EventSink?) {
+        eventSink = events!!
+      }
+
+      override fun onCancel(arguments: Any?) {
+        eventSink = null
+      }
+    })
+  }
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    onAttachedToEngine(flutterPluginBinding.binaryMessenger)
+  }
+
+  override fun onNewIntent(intent: Intent?): Boolean {
+    if (scheme == intent!!.data?.scheme) {
+      eventSink?.success(mapOf("type" to "url", "url" to intent.data?.toString()))
+    }
+
+    return true
+  }
+
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    methodChannel!!.setMethodCallHandler(null)
+    methodChannel = null
+
+    eventChannel!!.setStreamHandler(null)
+    methodChannel = null
+  }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activityPluginBinding = binding
+    binding.addOnNewIntentListener(this)
+  }
+
+  override fun onDetachedFromActivity() {
+    activityPluginBinding?.removeOnNewIntentListener(this)
+    activityPluginBinding = null
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    activityPluginBinding?.removeOnNewIntentListener(this)
+    activityPluginBinding = null
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    activityPluginBinding = binding
+    binding.addOnNewIntentListener(this)
   }
 }
