808dcdfc5d6c5579a3cc09a3d845838cbaa07f2b
0maru <0maru.dev@gmail.com>
2021-03-19 19:11:57 +0900

    logout用メソッド作った

--- ios/Classes/SwiftFlutterToricoIdLoginPlugin.swift
+++ ios/Classes/SwiftFlutterToricoIdLoginPlugin.swift
@@ -20,6 +20,8 @@ public class SwiftFlutterToricoIdLoginPlugin: NSObject, FlutterPlugin, ASWebAuth
     switch call.method {
     case "authentication":
       authentication(call, result: result)
+    case "logout":
+      logout(call, result: result)
     default:
       result(FlutterMethodNotImplemented)
       break
@@ -41,6 +43,60 @@ public class SwiftFlutterToricoIdLoginPlugin: NSObject, FlutterPlugin, ASWebAuth
       }
     }
     
+    if #available(iOS 12.0, *) {
+      // ASWebAuthenticationSession
+      var authSession: ASWebAuthenticationSession?
+      authSession = ASWebAuthenticationSession(
+        url: URL(string: url)!,
+        callbackURLScheme: urlScheme
+      ) { url, error in
+        result(url?.absoluteString)
+        authSession!.cancel()
+        self.session = nil
+      }
+      self.session = authSession
+      if #available(iOS 13.0, *) {
+        authSession?.presentationContextProvider = self
+      }
+      if !authSession!.start() {
+
+      }
+    } else if #available(iOS 11.0, *) {
+      // SFAuthenticationSession
+      var authSession: SFAuthenticationSession?
+      authSession = SFAuthenticationSession(
+        url: URL(string: url)!,
+        callbackURLScheme: urlScheme
+      ) { url, error in
+        result(url?.absoluteString)
+        authSession!.cancel()
+        self.session = nil
+      }
+      self.session = authSession
+      if !authSession!.start() {
+
+      }
+    } else if #available(iOS 9.0, *){
+      // SFSafariViewController
+      // Safari のCookieが使えないのでログイン情報の共有は出来ない
+      let url = URL(string: url)
+      if let url = url {
+        let safari = SFSafariController.init(url: url)
+        safari.open(url: url)
+        return
+      }
+    } else {
+      // iOS 9.0以前は未対応
+      result(nil)
+      return
+    }
+  }
+  
+  public func logout(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    let args = call.arguments as! NSDictionary
+    let url = args["url"] as! String
+    let urlScheme = args["redirectURI"] as! String
+
     if #available(iOS 12.0, *) {
       // ASWebAuthenticationSession
       var authSession: ASWebAuthenticationSession?
